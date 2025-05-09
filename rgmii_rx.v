8cce62f323bf4e418e3e50c2d53280d74a2a5be3
hongjh <121506322+hjh0920@users.noreply.github.com>
2025-05-09 09:01:03 +0000

    add rgmii_rx && rgmii_tx

--- rgmii_rx.v
+++ rgmii_rx.v
@@ -1,1 +1,193 @@
+// 本模块为以太网 RGMII 接收模块, 支持链路状态解析, 支持错误数据过滤
 
+module rgmii_rx(
+  // PHY 芯片状态指示
+  output      inband_link_status, // up(1), down(0)
+  output [1:0]inband_clock_speed, // 125MHz(10), 2.5MHz(01), 2.5MHz(00), reserved(11)
+  output      inband_duplex_status, // half-duplex(0), full-duplex(1)
+  // RGMII_RX
+  input        rgmii_rxc,
+  input        rgmii_rx_ctl,
+  input  [3:0] rgmii_rxd,
+  // User interface
+  output       rx_mac_aclk,
+  output [7:0] rx_axis_rgmii_tdata,
+  output       rx_axis_rgmii_tvalid
+);
+
+//------------------------------------
+//             Local Signal
+//------------------------------------
+  reg        link_status = 0; // up(1), down(0)
+  reg  [1:0] clock_speed = 0; // 125MHz(10), 25MHz(01), 2.5MHz(00), reserved(11)
+  reg        duplex_status = 0; // half-duplex(0), full-duplex(1)
+  
+  reg        nibble_flag = 0; // 10&100Mbps下每个周期翻转一次, 用于拼接数据
+  reg        gmii_rx_no_error = 0; // 接收数据正确标志
+  reg  [7:0] gmii_rxd_ff1 = 0; // 正确数据寄存器1
+  reg  [7:0] gmii_rxd_ff2 = 0; // 正确数据寄存器2
+  reg  [7:0] rx_axis_rgmii_tdata_ff = 0;
+  reg        rx_axis_rgmii_tvalid_ff = 0;
+  
+  // Instant Module Signal
+  wire       rgmii_rxc_bufio;
+  wire       rgmii_rxc_bufr;
+  wire       gmii_rx_dv;
+  wire       gmii_rx_er;
+  wire [7:0] gmii_rxd;
+
+//------------------------------------
+//             User Logic
+//------------------------------------
+// RGMII 2.0 支持直接通过 RX_DV & RX_ER & RXD[3:0] 来判断链路状态
+  // 当 RX_DV = 0 && RX_ER = 0 时, 通过 RXD[0] 判断链路链接状态
+  always @ (posedge rgmii_rxc_bufr)
+  begin
+    if ((gmii_rx_dv | gmii_rx_er) == 1'b0)
+      link_status <= gmii_rxd[0];
+    else
+      link_status <= link_status;
+  end
+  // 当 RX_DV = 0 && RX_ER = 0 时, 通过 RXD[2:1] 判断链路时钟状态
+  always @ (posedge rgmii_rxc_bufr)
+  begin
+    if ((gmii_rx_dv | gmii_rx_er) == 1'b0)
+      clock_speed <= gmii_rxd[2:1];
+    else
+      clock_speed <= clock_speed;
+  end
+  // 当 RX_DV = 0 && RX_ER = 0 时, 通过 RXD[3] 判断链路单双工状态
+  always @ (posedge rgmii_rxc_bufr)
+  begin
+    if ((gmii_rx_dv | gmii_rx_er) == 1'b0)
+      duplex_status <= gmii_rxd[3];
+    else
+      duplex_status <= duplex_status;
+  end
+// 接收数据正确标志
+  always @ (posedge rgmii_rxc_bufr)
+  begin
+    if (gmii_rx_dv)
+    // if (gmii_rx_dv & (~gmii_rx_er))
+      gmii_rx_no_error <= 1'b1;
+    else
+      gmii_rx_no_error <= 1'b0;
+  end
+// 寄存正确的数据
+  always @ (posedge rgmii_rxc_bufr)
+  begin
+    if (gmii_rx_dv)
+    // if (gmii_rx_dv & (~gmii_rx_er))
+      begin
+        gmii_rxd_ff1 <= gmii_rxd;
+        gmii_rxd_ff2 <= gmii_rxd_ff1;
+      end
+    else
+      begin
+        gmii_rxd_ff1 <= gmii_rxd_ff1;
+        gmii_rxd_ff2 <= gmii_rxd_ff2;
+      end
+  end
+// 10&100Mbps下每个周期翻转一次, 用于拼接数据
+  always @ (posedge rgmii_rxc_bufr)
+  begin
+    if (gmii_rx_no_error)
+      nibble_flag <= ~nibble_flag;
+    else
+      nibble_flag <= nibble_flag;
+  end
+// 接收数据及有效输出信号生成
+  always @ (posedge rgmii_rxc_bufr)
+  begin
+    if (link_status & gmii_rx_no_error)
+      begin
+        if (clock_speed == 2'b10) // 接收时钟为 125MHz, 对应链路速率 1Gbps
+          begin
+            rx_axis_rgmii_tdata_ff <= gmii_rxd_ff1;
+            rx_axis_rgmii_tvalid_ff <= 1'b1;
+          end
+        else if (nibble_flag) // 接收时钟为 2.5/25MHz, 对应链路速率 10/100Mbps, 且已接收完前半字节数据
+          begin
+            rx_axis_rgmii_tdata_ff <= {gmii_rxd_ff2[3:0],gmii_rxd_ff1[3:0]};
+            rx_axis_rgmii_tvalid_ff <= 1'b1;
+          end
+        else
+          begin
+            rx_axis_rgmii_tdata_ff <= 8'b0;
+            rx_axis_rgmii_tvalid_ff <= 1'b0;
+          end
+      end
+    else // 链路断开 或 接收数据错误
+      begin
+        rx_axis_rgmii_tdata_ff <= 8'b0;
+        rx_axis_rgmii_tvalid_ff <= 1'b0;
+      end
+  end
+
+//------------------------------------
+//             Output Port
+//------------------------------------
+  assign rx_mac_aclk = rgmii_rxc_bufr;
+  assign inband_link_status = link_status;
+  assign inband_clock_speed = clock_speed;
+  assign inband_duplex_status = duplex_status;
+  assign rx_axis_rgmii_tdata = rx_axis_rgmii_tdata_ff;
+  assign rx_axis_rgmii_tvalid = rx_axis_rgmii_tvalid_ff;
+
+//------------------------------------
+//             Instance
+//------------------------------------
+  BUFIO bufio_rgmii_rxc(
+    .O(rgmii_rxc_bufio), // 1-bit output: Clock output (connect to I/O clock loads).
+    .I(rgmii_rxc) // 1-bit input: Clock input (connect to an IBUF or BUFMR).
+  );
+
+  BUFR #(
+    .BUFR_DIVIDE("BYPASS"),   // Values: "BYPASS, 1, 2, 3, 4, 5, 6, 7, 8" 
+    .SIM_DEVICE("7SERIES")  // Must be set to "7SERIES" 
+  ) bufr_rgmii_rxc (
+    .O(rgmii_rxc_bufr),     // 1-bit output: Clock output port
+    .CE(1'b1),   // 1-bit input: Active high, clock enable (Divided modes only)
+    .CLR(1'b0), // 1-bit input: Active high, asynchronous clear (Divided modes only)
+    .I(rgmii_rxc)      // 1-bit input: Clock buffer input driven by an IBUF, MMCM or local interconnect
+  );
+
+  IDDR #(
+    .DDR_CLK_EDGE("SAME_EDGE_PIPELINED"), // "OPPOSITE_EDGE", "SAME_EDGE" 
+                                    //    or "SAME_EDGE_PIPELINED" 
+    .INIT_Q1(1'b0), // Initial value of Q1: 1'b0 or 1'b1
+    .INIT_Q2(1'b0), // Initial value of Q2: 1'b0 or 1'b1
+    .SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC" 
+  ) iddr_rgmii_rx_ctl (
+    .Q1(gmii_rx_dv), // 1-bit output for positive edge of clock
+    .Q2(gmii_rx_er), // 1-bit output for negative edge of clock
+    .C(rgmii_rxc_bufio),   // 1-bit clock input
+    .CE(1'b1), // 1-bit clock enable input
+    .D(rgmii_rx_ctl),   // 1-bit DDR data input
+    .R(1'b0),   // 1-bit reset, active high
+    .S(1'b0)    // 1-bit set, active high
+  );
+  
+genvar i;
+generate
+  for (i = 0; i < 4; i = i + 1) begin
+    IDDR #(
+      .DDR_CLK_EDGE("SAME_EDGE_PIPELINED"), // "OPPOSITE_EDGE", "SAME_EDGE" 
+                                            //    or "SAME_EDGE_PIPELINED" 
+      .INIT_Q1(1'b0), // Initial value of Q1: 1'b0 or 1'b1
+      .INIT_Q2(1'b0), // Initial value of Q2: 1'b0 or 1'b1
+      .SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC" 
+    ) iddr_rgmii_rxd (
+      .Q1(gmii_rxd[i]), // 1-bit output for positive edge of clock
+      .Q2(gmii_rxd[i+4]), // 1-bit output for negative edge of clock
+      .C(rgmii_rxc_bufio),   // 1-bit clock input
+      .CE(1'b1), // 1-bit clock enable input
+      .D(rgmii_rxd[i]),   // 1-bit DDR data input
+      .R(1'b0),   // 1-bit reset, active high
+      .S(1'b0)    // 1-bit set, active high
+    );
+  end
+endgenerate
+
+
+endmodule
---
+++ rgmii_tx.v
@@ -0,0 +1,322 @@
+// 本模块为以太网RGMII发送模块, 支持速率自适应
+
+module rgmii_tx (
+  input         clk_125mhz,
+  input         clk90_125mhz,
+  input         reset,
+  input         reset90,
+  // PHY 芯片状态指示
+  input         phy_link_status, // up(1), down(0)
+  input  [1:0]  phy_speed_status, // 10Mbps(0), 100Mbps(1), 1000Mbps(2)
+  // RGMII_TX
+  output        rgmii_txc,
+  output        rgmii_tx_ctl,
+  output [3:0]  rgmii_txd,
+  // User interface
+  input  [7:0]  tx_axis_rgmii_tdata,
+  input         tx_axis_rgmii_tvalid,
+  output        tx_axis_rgmii_tready
+);
+
+//------------------------------------
+//             Local Signal
+//------------------------------------
+  wire          phy_link_status_txclk;
+  wire [1:0]    phy_speed_status_txclk;
+  reg  [7:0]    tx_axis_rgmii_tdata_ff = 0; // 寄存信号
+  reg           tx_axis_rgmii_handshake = 0; // 握手成功指示
+  reg  [5:0]    clk_cnt = 0; // 分频时钟计数器
+  reg           clk_div5_50 = 1; // 10/100Mbps下 5/50倍分频信号
+  reg           clk90_div5_50 = 0; // 10/100Mbps下 5/50倍分频信号并相移90度信号
+  reg           tx_data_en = 0; // 发送数据有效指示
+  reg           tx_data_error = 0; // 发送数据错误指示
+  reg           tx_axis_rgmii_tready_ff = 0; // RGMII发送准备信号
+  reg  [3:0]    tx_data_msb = 0; // RGMII 发送数据高 4-bit
+  reg  [3:0]    tx_data_lsb = 4'b1101; // RGMII 发送数据低 4-bit
+  reg           tx_nibble_sw = 0; // 10/100Mbps下 RGMII 发送半字节切换指示
+  reg           tx_nibble_sw_d1 = 0; // 延迟一拍
+  reg           msb_lsb_flag = 0; // 10/100Mbps下当前 RGMII  发送高低nibble指示, H-nibble(1), L-nibble(0)
+  reg           bus_status = 0; // 发送总线状态, 空闲(0), 忙碌(1)
+  reg           tx10_100_data_en = 0; // 10/100Mbps有效数据发送期间指示
+  reg           tx10_100_data_en_d1 = 0; // 延迟一拍
+
+//------------------------------------
+//             User Logic
+//------------------------------------
+// 分频时钟计数器
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset | (~phy_link_status_txclk))
+      clk_cnt <= 6'd0;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps
+      clk_cnt <= 6'd0;
+    else if (phy_speed_status_txclk[0]) // 100Mbps
+      begin
+        if (clk_cnt == 6'd4)
+          clk_cnt <= 6'd0;
+        else
+          clk_cnt <= clk_cnt + 6'd1;
+      end
+    else // 10Mbps
+      begin
+        if (clk_cnt == 6'd49)
+          clk_cnt <= 6'd0;
+        else
+          clk_cnt <= clk_cnt + 6'd1;
+      end
+  end
+// 10/1000Mbps下5/50倍分频信号
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset | (~phy_link_status_txclk))
+      clk_div5_50 <= 1'b1;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps
+      clk_div5_50 <= 1'b1;
+    else if (phy_speed_status_txclk[0]) // 100Mbps
+      begin
+        if (clk_cnt == 6'd4 || clk_cnt == 6'd0)
+          clk_div5_50 <= 1'b1;
+        else
+          clk_div5_50 <= 1'b0;
+      end
+    else // 10Mbps
+      begin
+        if (clk_cnt == 6'd49 || clk_cnt < 6'd24)
+          clk_div5_50 <= 1'b1;
+        else
+          clk_div5_50 <= 1'b0;
+      end
+  end
+// RGMII 发送数据AXIS接口握手成功指示
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      tx_axis_rgmii_handshake <= 1'b0;
+    else if (tx_axis_rgmii_tvalid & tx_axis_rgmii_tready_ff) // 握手成功
+      tx_axis_rgmii_handshake <= 1'b1;
+    else
+      tx_axis_rgmii_handshake <= 1'b0;
+  end
+// RGMII发送数据寄存
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      tx_axis_rgmii_tdata_ff <= {4'b0,4'b1101};
+    else if (tx_axis_rgmii_tvalid & tx_axis_rgmii_tready_ff) // 更新发送数据
+      tx_axis_rgmii_tdata_ff <= tx_axis_rgmii_tdata;
+    else if (tx_nibble_sw) // 10/100Mbps 发送高低nibble
+      tx_axis_rgmii_tdata_ff <= {4'b0,tx_axis_rgmii_tdataff[7:4]};
+    else if (!tx10_100_data_en) // 默认传输数据
+      tx_axis_rgmii_tdata_ff <= {4'b0,1'b1,phy_speed_status_txclk,1'b1};
+    else
+      tx_axis_rgmii_tdata_ff <= tx_axis_rgmii_tdata_ff;
+  end
+// 发送总线状态, 空闲(0), 忙碌(1)
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      bus_status <= 1'b0;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps
+      bus_status <= 1'b1;
+    else // 10/100Mbps
+      begin
+        if (tx_axis_rgmii_handshake) // 有新数据
+          bus_status <= 1'b1;
+        else if (msb_lsb_flag && ((phy_speed_status_txclk[0] && clk_cnt == 6'd3) || ((!phy_speed_status_txclk[0]) && clk_cnt == 6'd48))) // 发送完当前数据, 且没有新数据待发送
+          bus_status <= 1'b0;
+        else
+          bus_status <= bus_status;
+      end
+  end
+// RGMII发送准备信号
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset || (~phy_link_status_txclk))
+      tx_axis_rgmii_tready_ff <= 1'b0;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps
+      tx_axis_rgmii_tready_ff <= 1'b1;
+    else // 10/100Mbps
+      begin
+        if (tx_axis_rgmii_tvalid && ((!bus_status) | msb_lsb_flag) && ((phy_speed_status_txclk[0] && clk_cnt == 6'd2) || ((!phy_speed_status_txclk[0]) && clk_cnt == 6'd47))) // 10/100Mbps下非连续输入发送数据或发送完当前数据, 且还有数据待发送
+          tx_axis_rgmii_tready_ff <= 1'b1;
+        else
+          tx_axis_rgmii_tready_ff <= 1'b0;
+      end
+  end
+// 发送数据有效指示
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      tx_data_en <= 1'b0;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps
+      tx_data_en <= tx_axis_rgmii_handshake;
+    else // 10/100Mbps
+      begin
+        if (tx_axis_rgmii_handshake | tx_nibble_sw_d1) // 有新数据 或 发送完低nibble, 准备发送高nibble
+          tx_data_en <= 1'b1;
+        else if ((phy_speed_status_txclk[0] && clk_cnt == 6'd4) || ((!phy_speed_status_txclk[0]) && clk_cnt == 6'd49)) // 发完半个周期使能信号, 接着发送半个周期错误指示信号
+          tx_data_en <= 1'b0;
+        else
+          tx_data_en <= tx_data_en;
+      end
+  end
+// 发送数据错误指示
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      tx_data_error <= 1'b0;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps
+      tx_data_error <= 1'b0;
+    else // 10/100Mbps
+      begin
+        if (tx_axis_rgmii_handshake | tx_nibble_sw_d1) // 有新数据 或 发送完低nibble, 准备发送高nibble
+          tx_data_error <= 1'b1;
+        else if ((phy_speed_status_txclk[0] && clk_cnt == 6'd2) || ((!phy_speed_status_txclk[0]) && clk_cnt == 6'd24)) // 发完半个周期使能信号, 接着发送半个周期错误指示信号
+          tx_data_error <= 1'b0;
+        else
+          tx_data_error <= tx_data_error;
+      end
+  end
+// 10/100Mbps下 RGMII 发送半字节切换指示
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      tx_nibble_sw <= 1'b0;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps
+      tx_nibble_sw <= 1'b0;
+    else if (bus_status && (!msb_lsb_flag))
+      begin
+        if (phy_speed_status_txclk[0] && clk_cnt == 6'd2) // 100Mbps
+          tx_nibble_sw <= 1'b1;
+        else if ((!phy_speed_status_txclk[0]) && clk_cnt == 6'd47) // 10Mbps
+          tx_nibble_sw <= 1'b1;
+        else
+          tx_nibble_sw <= 1'b0;
+      end
+    else
+      tx_nibble_sw <= 1'b0;
+  end
+// 延迟一拍
+  always @ (posedge clk_125mhz)
+  begin
+    tx_nibble_sw_d1 <= tx_nibble_sw;
+    tx10_100_data_en_d1 <= tx10_100_data_en;
+  end
+// 10/100Mbps下当前 RGMII  发送高低nibble指示, H-nibble(1), L-nibble(0)
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      msb_lsb_flag <= 1'b0;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps
+      msb_lsb_flag <= 1'b0;
+    else if (bus_status) // 10/100Mbps
+      begin
+        if ((phy_speed_status_txclk[0] && clk_cnt == 6'd4) || ((!phy_speed_status_txclk[0]) && clk_cnt == 6'd49))
+          msb_lsb_flag <= ~msb_lsb_flag;
+        else
+          msb_lsb_flag <= msb_lsb_flag;
+      end
+    else
+      msb_lsb_flag <= 1'b0;
+  end
+
+// 10/100Mbps有效数据发送期间指示
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      tx10_100_data_en <= 1'b0;
+    else if (!phy_speed_status_txclk[1]) // 10/100Mbps
+      begin
+        if (tx_axis_rgmii_tready_ff | tx_nibble_sw) // 有新数据 或 发送完低nibble, 准备发送高nibble
+        else if ((phy_speed_status_txclk[0] && clk_cnt == 6'd3) || ((!phy_speed_status_txclk[0]) && clk_cnt == 6'd48))
+          tx10_100_data_en <= 1'b0;
+        else
+          tx10_100_data_en <= tx10_100_data_en;
+      end
+    else
+      tx10_100_data_en <= 1'b0;
+  end
+// 发送数据高nibble
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset)
+      tx_data_msb <= 4'b0;
+    else if (phy_speed_status_txclk[1]) // 1000Mbps 下双沿传输
+      tx_data_msb <= tx_axis_rgmii_tdata_ff[7:4];
+    else // 10/100Mbps 下单沿传输
+      tx_data_msb <= tx_axis_rgmii_tdata_ff[3:0];
+  end
+// 发送数据低nibble
+  always @ (posedge clk_125mhz)
+  begin
+    if (reset || (!phy_link_status_txclk))
+      tx_data_lsb <= 4'b1101;
+    else
+      tx_data_lsb <= tx_axis_rgmii_tdata_ff[3:0];
+  end
+//------------------------------------
+//             Output Port
+//------------------------------------
+  assign tx_axis_rgmii_tready = tx_axis_rgmii_tready_ff;
+
+//------------------------------------
+//             Instance
+//------------------------------------
+
+  ODDR #(
+    .DDR_CLK_EDGE("SAME_EDGE"), // "OPPOSITE_EDGE" or "SAME_EDGE" 
+    .INIT(1'b0),    // Initial value of Q: 1'b0 or 1'b1
+    .SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC" 
+  ) oddr_rgmii_txc (
+    .Q(rgmii_txc),   // 1-bit DDR output
+    .C(clk_125mhz),   // 1-bit clock input
+    .CE(1'b1), // 1-bit clock enable input
+    .D1(clk_div5_50), // 1-bit data input (positive edge)
+    .D2(clk90_div5_50), // 1-bit data input (negative edge)
+    .R(reset),   // 1-bit reset
+    .S(1'b0)    // 1-bit set
+  );
+
+  ODDR #(
+    .DDR_CLK_EDGE("SAME_EDGE"), // "OPPOSITE_EDGE" or "SAME_EDGE" 
+    .INIT(1'b0),    // Initial value of Q: 1'b0 or 1'b1
+    .SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC" 
+  ) oddr_rgmii_tx_ctl (
+    .Q(rgmii_tx_ctl),   // 1-bit DDR output
+    .C(clk_125mhz),   // 1-bit clock input
+    .CE(1'b1), // 1-bit clock enable input
+    .D1(tx_data_en), // 1-bit data input (positive edge)
+    .D2(tx_data_en), // 1-bit data input (negative edge)
+    // .D2(tx_data_error), // 1-bit data input (negative edge)
+    .R(reset),   // 1-bit reset
+    .S(1'b0)    // 1-bit set
+  );
+
+genvar i;
+generate
+  for (i = 0; i < 4; i= i + 1)
+    begin
+      ODDR #(
+        .DDR_CLK_EDGE("SAME_EDGE"), // "OPPOSITE_EDGE" or "SAME_EDGE" 
+        .INIT(1'b0),    // Initial value of Q: 1'b0 or 1'b1
+        .SRTYPE("SYNC") // Set/Reset type: "SYNC" or "ASYNC" 
+      ) oddr_rgmii_txd (
+        .Q(rgmii_txd[i]),   // 1-bit DDR output
+        .C(clk_125mhz),   // 1-bit clock input
+        .CE(1'b1), // 1-bit clock enable input
+        .D1(tx_data_lsb[i]), // 1-bit data input (positive edge)
+        .D2(tx_data_msb[i]), // 1-bit data input (negative edge)
+        .R(reset),   // 1-bit reset
+        .S(1'b0)    // 1-bit set
+      );
+    end
+endgenerate
+
+  xpm_cdc_array_sigle #
+
+
+
+  xpm_cdc_single #
+
+
+endmodule
