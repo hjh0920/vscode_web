711cd160b1256cc5306a9786240420e50b7ac198
hongjh <121506322+hjh0920@users.noreply.github.com>
2025-06-09 09:07:42 +0000

    111

--- USB3.0/axis_width_converter.v
+++ USB3.0/axis_width_converter.v
@@ -1,3 +1,4 @@
+// AXI4-Stream位宽转换模块
 
 module axis_width_converter #(
   parameter interger S_TDATA_WIDTH = 0, // 1-512 (byte)
@@ -28,7 +29,29 @@ module axis_width_converter #(
   input  [TID_WIDTH-1:0]                          m_axis_tid,
   input  [TDEST_WIDTH-1:0]                        m_axis_tdest,
   input  [M_TDATA_WIDTH*TUSER_WIDTH_PER_BYTE-1:0] m_axis_tuser
-
 );
   
+generate
+  if (S_TDATA_WIDTH == M_TDATA_WIDTH)
+    begin
+      assign m_axis_tvalid = s_axis_tvalid;
+      assign s_axis_tready = m_axis_tready;
+      assign m_axis_tdata = s_axis_tdata;
+      assign m_axis_tstrb = s_axis_tstrb;
+      assign m_axis_tkeep = s_axis_tkeep;
+      assign m_axis_tlast = s_axis_tlast;
+      assign m_axis_tid = s_axis_tid;
+      assign m_axis_tdest = s_axis_tdest;
+      assign m_axis_tuser = s_axis_tuser;
+    end
+  else if (S_TDATA_WIDTH < M_TDATA_WIDTH)
+    begin
+
+    end
+  else // S_TDATA_WIDTH > M_TDATA_WIDTH
+    begin
+      
+    end
+endgenerate
+
 endmodule
